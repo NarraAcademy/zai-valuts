aac7c807692c68c7fdbcc2b1b0ff93240399150d
success_mac <success_mac@success-macs-MacBook-Air.local>
2025-06-30 12:27:52 +0200

    update staking token

--- src/token/StakingToken.sol
+++ src/token/StakingToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract StakingToken is ERC20, Ownable {
-    constructor() ERC20("NarraLayer StakingToken", "NSTK") Ownable(msg.sender) {}
+    constructor() ERC20("No Airdrop No Alpha", "NANA") Ownable(msg.sender) {}
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
