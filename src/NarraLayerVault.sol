abe8544006add59a296e76016c5ca80d3071353a
success_mac <success_mac@success-macs-MacBook-Air.local>
2025-07-08 13:41:15 -0400

    add non-zero check

--- src/NarraLayerVault.sol
+++ src/NarraLayerVault.sol
@@ -146,7 +146,9 @@ contract NarraLayerVault is
         address token,
         uint256 _weightPerToken
     ) external override onlyRole(ADMIN_ROLE) {
+        require(token != address(0), "Token address cannot be zero");
         require(token != stakingTokenAddress, "Cannot add staking token as supported token");
+        require(_weightPerToken > 0, "Weight must be greater than zero");
         supportedTokens[token] = _weightPerToken;
         emit SupportedTokenUpdated(token, _weightPerToken);
     }
@@ -165,6 +167,7 @@ contract NarraLayerVault is
     function removeSupportedToken(
         address token
     ) external override onlyRole(ADMIN_ROLE) {
+        require(token != address(0), "Token address cannot be zero");
         require(token != stakingTokenAddress, "Cannot remove staking token as supported token");
         delete supportedTokens[token];
         emit SupportedTokenUpdated(token, 0);
@@ -273,6 +276,7 @@ contract NarraLayerVault is
     }
 
     function cleanExpiredStakes(uint256 maxCount) external override {
+        require(maxCount > 0, "Max count must be greater than zero");
         _cleanExpiredStakes(maxCount);
     }
 
